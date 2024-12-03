bf9f04d17ec1aaacae4f82b8f8c368fae3466b01
Matt Costuros <matt.costuros@gmail.com>
2024-12-03 18:15:03 -0500

    init

---
+++ src/AOC2024/SampleDay.cs
@@ -0,0 +1,14 @@
+// public class Day01 : BaseDay
+// {
+//     private readonly string _input;
+
+//     public Day01()
+//     {
+//         _input = File.ReadAllText(InputFilePath);
+//     }
+
+//     public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+
+//     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");    
+
+// }
