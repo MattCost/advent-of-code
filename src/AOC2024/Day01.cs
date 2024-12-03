68c8bc54648c12d90704598c5933291e9ac3caf9
Matt Costuros <matt.costuros@gmail.com>
2024-12-03 18:48:39 -0500

    day 1 complete

--- src/AOC2024/Day01.cs
+++ src/AOC2024/Day01.cs
@@ -1,14 +1,50 @@
+using System.Collections;
+
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    List<int> Left = new();
+    List<int> Right = new();
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        StreamReader sr = new StreamReader(InputFilePath);
+
+        string? line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            var numbers = line.Split("   ");
+            Left.Add(int.Parse(numbers[0]));
+            Right.Add(int.Parse(numbers[1]));
+        }
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var leftItr = Left.Order().GetEnumerator();
+        var rightItr = Right.Order().GetEnumerator();
+        long total = 0;
+        do
+        {
+            total += Math.Abs(leftItr.Current - rightItr.Current);
+        }
+        while (leftItr.MoveNext() && rightItr.MoveNext());
+        return new(total.ToString());
+    }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");    
+    public override ValueTask<string> Solve_2()
+    {
+        Dictionary<int, int> _tracking = new();
+        long total = 0;
+        for (int i = 0; i < Left.Count; i++)
+        {
+            var j = Left[i];
+            if (!_tracking.ContainsKey(j))  // this seems to make no speed diff, linq caching is nice
+            {
+                _tracking[j] = Right.Where(x => x == j).Count();
+            }
+            total += j * _tracking[j];
+        }
+        return new(total.ToString());
+    }
 
 }
