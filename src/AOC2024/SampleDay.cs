a6e15ecb19fbeab632d6593a595985811cad4d86
Matt Costuros <matt.costuros@gmail.com>
2024-12-04 17:58:36 -0500

    sample updatge

--- src/AOC2024/SampleDay.cs
+++ src/AOC2024/SampleDay.cs
@@ -1,14 +1,27 @@
-// public class Day01 : BaseDay
+// public class Day02 : BaseDay
 // {
-//     private readonly string _input;
+//     List<string> _lines = new();
 
-//     public Day01()
+//     public DayX()
 //     {
-//         _input = File.ReadAllText(InputFilePath);
+//         StreamReader sr = new StreamReader(InputFilePath);
+
+//         string? line;
+//         while ((line = sr.ReadLine()) != null)
+//         {
+//             _lines.Add(line);
+//         }
+
 //     }
 
-//     public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+//     public override ValueTask<string> Solve_1()
+//     {
+//         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+//     } 
 
-//     public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");    
+//     public override ValueTask<string> Solve_2()
+//     {
+//         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");    
+//     } 
 
 // }
