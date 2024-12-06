118ac51cb154ecf319f376cf700bfc668b60ed7f
Matt Costuros <matt.costuros@gmail.com>
2024-12-06 16:58:54 -0500

    day 5 - part 2

--- src/AOC2024/Day05.cs
+++ src/AOC2024/Day05.cs
@@ -1,3 +1,4 @@
+
 public class Day05 : BaseDay
 {
     Dictionary<int, List<int>> _orderRules = new();
@@ -54,7 +55,6 @@ public class Day05 : BaseDay
             }
             if (valid)
             {
-                Console.WriteLine($"Order {string.Join(' ', order)} is valid");
                 var midpoint = order[order.Count / 2];
                 output += midpoint;
             }
@@ -65,7 +65,62 @@ public class Day05 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        int output = 0;
+        foreach (var order in _orders)
+        {
+            var valid = true;
+            for (int i = 1; i < order.Count; i++)
+            {
+                var pre = order.GetRange(0, i);
+                var current = order[i];
+                if (!_orderRules.ContainsKey(current)) continue;
+                var rule = _orderRules[current];
+                if (rule.Select(ruleEntry => pre.Contains(ruleEntry)).Where(x => x == true).Any())
+                {
+                    //Invalid rule
+                    valid = false;
+                    break;
+                }
+            }
+            if (!valid)
+            {
+                Console.WriteLine($"Order {string.Join(' ', order)} is invalid");
+                var sortedOrder = FixOrder(order);
+                Console.WriteLine($"Fixed order {string.Join(' ', sortedOrder)} ");
+
+                var midpoint = sortedOrder[sortedOrder.Count / 2];
+                output += midpoint;
+            }
+
+        }
+        return new(output.ToString());
     }
 
+    private List<int> FixOrder(List<int> order)
+    {
+        var output = new List<int>();
+        output.Add(order.First());
+        for (int i = 1; i < order.Count; i++)
+        {
+            var current = order[i];
+            if (!_orderRules.ContainsKey(current))
+            {
+                //if no rules that we must be before someone else, add to end
+                output.Add(current);
+                continue;
+            }
+            var rule = _orderRules[current];
+            var anyIssues = rule.Select(ruleEntry => output.IndexOf(ruleEntry)).Where(i => i != -1);
+            if (anyIssues.Any())
+            {
+                var target = anyIssues.Min();
+                output.Insert(target, current);
+            }
+            else
+            {
+                output.Add(current);
+            }
+        }
+        return output;
+    }
 }
