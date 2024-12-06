901398296c8b7a745889f39470b876f94d8fdf43
Matt Costuros <matt.costuros@gmail.com>
2024-12-06 08:55:45 -0500

    day 4 part 1

---
+++ src/AOC2024/Day04.cs
@@ -0,0 +1,91 @@
+using System.Runtime.InteropServices.Marshalling;
+using AdventOfCode.Base.Grid8Way;
+
+public class Day04 : BaseDay
+{
+    List<string> _lines = new();
+    int _rows;
+    int _cols;
+    Grid8Way<char> _grid;
+
+    public Day04()
+    {
+        StreamReader sr = new StreamReader(InputFilePath);
+
+        string? line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            _lines.Add(line);
+        }
+
+        _rows = _lines.Count();
+        _cols = _lines[0].Length;
+        _grid = new Grid8Way<char>(_rows, _cols);
+
+        for (int row = 0; row < _rows; row++)
+        {
+            for (int col = 0; col < _cols; col++)
+            {
+                _grid.AddNode(_lines[row][col], row, col);
+            }
+        }
+
+        _grid.PrintGrid();
+        _grid.PopulateEdges();
+
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        long output = 0;
+        int searchCount = 0;
+        for (int row = 0; row < _rows; row++)
+        {
+            for (int col = 0; col < _cols; col++)
+            {
+                if (_grid.Nodes[row, col] != null && _grid.Nodes[row, col].Value == 'X')
+                {
+                    searchCount++;
+                    output += SearchResult(row, col);
+                }
+            }
+        }
+
+        Console.WriteLine($"Searched {searchCount} spots and found {output} xmas");
+        return new(output.ToString());
+    }
+
+    private long SearchResult(int row, int col)
+    {
+        int output = 0;
+
+        // Brute forcing the search, as it was only 8 directions, and 4 chars, so we didn't need to get recursive. Also, the linked matrix was totally unnecessary.
+        // Left
+        if (col >= 3 && _grid.Nodes[row, col - 1].Value == 'M' && _grid.Nodes[row, col - 2].Value == 'A' && _grid.Nodes[row, col - 3].Value == 'S') output++;
+        // UpLeft
+        if (col >= 3 && row >= 3 && _grid.Nodes[row - 1, col - 1].Value == 'M' && _grid.Nodes[row - 2, col - 2].Value == 'A' && _grid.Nodes[row - 3, col - 3].Value == 'S') output++;
+        // DownLeft
+        if (col >= 3 && row <= _rows - 4 && _grid.Nodes[row + 1, col - 1].Value == 'M' && _grid.Nodes[row + 2, col - 2].Value == 'A' && _grid.Nodes[row + 3, col - 3].Value == 'S') output++;
+
+        // Right
+        if (col <= _cols - 4 && _grid.Nodes[row, col + 1].Value == 'M' && _grid.Nodes[row, col + 2].Value == 'A' && _grid.Nodes[row, col + 3].Value == 'S') output++;
+        // UpRight
+        if (col <= _cols - 4 && row >= 3 && _grid.Nodes[row - 1, col + 1].Value == 'M' && _grid.Nodes[row - 2, col + 2].Value == 'A' && _grid.Nodes[row - 3, col + 3].Value == 'S') output++;
+        // DownRight
+        if (col <= _cols - 4 && row <= _rows - 4 && _grid.Nodes[row + 1, col + 1].Value == 'M' && _grid.Nodes[row + 2, col + 2].Value == 'A' && _grid.Nodes[row + 3, col + 3].Value == 'S') output++;
+
+        // Up
+        if (row >= 3 && _grid.Nodes[row - 1, col].Value == 'M' && _grid.Nodes[row - 2, col].Value == 'A' && _grid.Nodes[row - 3, col].Value == 'S') output++;
+
+        // Down
+        if (row <= _rows - 4 && _grid.Nodes[row + 1, col].Value == 'M' && _grid.Nodes[row + 2, col].Value == 'A' && _grid.Nodes[row + 3, col].Value == 'S') output++;
+
+        return output;
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    }
+
+}
---
+++ src/Base/Grid8Way/Grid8Way.cs
@@ -0,0 +1,150 @@
+namespace AdventOfCode.Base.Grid8Way
+{
+    public enum Direction
+    {
+        None,
+        Up,
+        Down,
+        Left,
+        Right,
+        UpLeft,
+        UpRight,
+        DownLeft,
+        DownRight
+    }
+
+    public class Grid8WayNode<T>
+    {
+        public T Value { get; init; }
+        public int Row { get; init; }
+        public int Col { get; init; }
+
+        public Grid8WayNode(T value, int row, int col)
+        {
+            Value = value;
+            Row = row;
+            Col = col;
+        }
+
+        public List<Grid8WayEdge<T>> Edges { get; init; } = new();
+    }
+
+    public class Grid8WayEdge<T>
+    {
+        public Direction Direction { get; init; }
+        public Grid8WayNode<T> Node { get; init; }
+        public Grid8WayEdge(Grid8WayNode<T> node, Direction direction)
+        {
+            Direction = direction;
+            Node = node;
+        }
+
+    }
+    public class Grid8Way<T>
+    {
+        public Grid8WayNode<T>[,] Nodes { get; init; }
+        public int Rows { get; init; }
+        public int Cols { get; init; }
+
+        public Grid8Way(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Nodes = new Grid8WayNode<T>[rows, cols];
+        }
+
+        public void AddNode(T value, int row, int col)
+        {
+            Nodes[row,col] = new Grid8WayNode<T>(value, row, col);
+        }
+
+
+
+        public void PrintGrid()
+        {
+            for(int r = 0 ; r < Rows ; r++)
+            {
+                for( int c=0; c<Cols ; c++)
+                {
+                    Console.Write($"{Nodes[r,c].Value} ");
+                }
+                Console.Write("\n");
+            }
+        }
+        public void PopulateEdges()
+        {
+            // Link up the nodes
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Cols; col++)
+                {
+                    if(Nodes[row,col] == null) 
+                    {
+                        continue;
+                    }
+
+                    // Ups
+                    if (row > 0)
+                    {
+                        Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row - 1, col], Direction.Up));
+
+                        if (col > 0)
+                        {
+                            Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row - 1, col - 1], Direction.UpLeft));
+                        }
+                        if (col < Cols - 1)
+                        {
+                            Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row - 1, col + 1], Direction.UpRight));
+                        }
+                    }
+
+                    // Downs
+                    if (row < Rows - 1)
+                    {
+                        Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row + 1, col], Direction.Down));
+
+                        if (col > 0)
+                        {
+                            Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row + 1, col - 1], Direction.DownLeft));
+                        }
+                        if (col < Cols - 1)
+                        {
+                            Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row + 1, col + 1], Direction.DownRight));
+                        }
+                    }
+
+                    // Lefts
+                    if (col > 0)
+                    {
+                        Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row, col - 1], Direction.Left));
+
+                        if (row > 0)
+                        {
+                            Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row - 1, col - 1], Direction.UpLeft));
+                        }
+                        if (row < Rows - 1)
+                        {
+                            Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row + 1, col - 1], Direction.DownLeft));
+                        }
+                    }
+
+                    // Rights
+                    if (col < Cols - 1)
+                    {
+                        Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row, col + 1], Direction.Right));
+
+                        if (row > 0)
+                        {
+                            Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row - 1, col + 1], Direction.UpRight));
+                        }
+                        if (row < Rows - 1)
+                        {
+                            Nodes[row, col].Edges.Add(new Grid8WayEdge<T>(Nodes[row + 1, col + 1], Direction.DownRight));
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
