bb93d0ddc1480fd8b32e893ca7e3ee64da7b50cf
Matt Costuros <matt.costuros@gmail.com>
2024-12-12 12:44:23 -0500

    Extension methods to turn right

--- src/Base/Grid/GridDirections.cs
+++ src/Base/Grid/GridDirections.cs
@@ -7,4 +7,32 @@ namespace AdventOfCode.Base.Grid
         Left,
         Right,
     }
+
+    public static class DirectionExtensions
+    {
+        public static Direction TurnRight(this Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Up => Direction.Right,
+                Direction.Right => Direction.Down,
+                Direction.Down => Direction.Left,
+                Direction.Left => Direction.Up,
+                _ => throw new Exception("missed a case")
+            };
+        }
+
+        public static Direction TurnLeft(this Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Up => Direction.Left,
+                Direction.Left => Direction.Down,
+                Direction.Down => Direction.Right,
+                Direction.Right => Direction.Up,
+                _ => throw new Exception("missed a case")
+            };
+        }        
+
+    }
 }
