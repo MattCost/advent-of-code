79dec46bd4cdf330c7be08ac4528c20daf062eff
Matt Costuros <matt.costuros@gmail.com>
2024-12-12 12:59:14 -0500

    Solved puzzle and sample input, but solution is not robust

--- src/AOC2024/Day12.cs
+++ src/AOC2024/Day12.cs
@@ -34,7 +34,7 @@ public class Day12 : BaseDay
 
         foreach (var plotType in _plotInfo.Keys.ToList())
         {
-            Console.WriteLine($"Working on plotType {plotType}");
+            // Console.WriteLine($"Working on plotType {plotType}");
             for (int r = 0; r < _rows; r++)
             {
                 for (int c = 0; c < _cols; c++)
@@ -43,10 +43,10 @@ public class Day12 : BaseDay
                     var node = _map.Nodes[r, c];
                     if (node.Value == plotType && !node.Visited)
                     {
-                        Console.WriteLine($"Found a plot of type {plotType} at [{r},{c}] not yet visited.");
+                        // Console.WriteLine($"Found a plot of type {plotType} at [{r},{c}] not yet visited.");
                         // find extents of plot
                         var result = FindPlotExtents(node);
-                        Console.WriteLine($"This plot has an area of {result.area} a perimeter of {result.perimeter} and a {result.sides} sides");
+                        // Console.WriteLine($"This plot has an area of {result.area} a perimeter of {result.perimeter} and a {result.sides} sides");
                         _plotInfo[node.Value].Add(result);
                     }
 
@@ -85,23 +85,23 @@ public class Day12 : BaseDay
         long perimeter = nodesInPlot.Select(node => 4 - node.Edges.Count).Sum();
 
         //** Walk the perimeter to count sides
-        Console.WriteLine($"Starting perimeter walk for plot type {node.Value}");
+        // Console.WriteLine($"Starting perimeter walk for plot type {node.Value}");
         List<GridNode<char>> _walkedNodes = new();
         int sides = 0;
         foreach (var nodeInPlot in nodesInPlot)
         {
-            Console.WriteLine($"Checking node [{nodeInPlot.Row},{nodeInPlot.Col}] {nodeInPlot.Value}");
+            // Console.WriteLine($"Checking node [{nodeInPlot.Row},{nodeInPlot.Col}] {nodeInPlot.Value}");
             // If node is not a border, there is no perimeter to walk.
             if (nodeInPlot.Edges.Count == 4)
             {
-                Console.WriteLine("Node has 4 edges, not a border");
+                // Console.WriteLine("Node has 4 edges, not a border");
                 continue;
             }
 
             // If we have already walked this node as part of a previous perimeter walk, move on
             if (_walkedNodes.Contains(nodeInPlot))
             {
-                Console.WriteLine("We have already walked here");
+                // Console.WriteLine("We have already walked here");
                 continue;
             }
 
@@ -109,30 +109,30 @@ public class Day12 : BaseDay
             if (!nodeInPlot.Edges.Where(edge => edge.Direction == Direction.Down).Any())
             {
                 //on bottom edge, so go left
-                Console.WriteLine("No edge going down, starting off left");
+                // Console.WriteLine("No edge going down, starting off left");
                 direction = Direction.Left;
             }
             else if (!nodeInPlot.Edges.Where(edge => edge.Direction == Direction.Left).Any())
             {
                 //on left edge, so go up
-                Console.WriteLine("No edge going left, starting off up");
+                // Console.WriteLine("No edge going left, starting off up");
                 direction = Direction.Up;
             }
             else if (!nodeInPlot.Edges.Where(edge => edge.Direction == Direction.Up).Any())
             {
                 // on top edge, so go right
-                Console.WriteLine("No edge going up, starting off right");
+                // Console.WriteLine("No edge going up, starting off right");
                 direction = Direction.Right;
             }
             else if (!nodeInPlot.Edges.Where(edge => edge.Direction == Direction.Right).Any())
             {
-                Console.WriteLine("No edge going right, starting off down");
+                // Console.WriteLine("No edge going right, starting off down");
                 direction = Direction.Down;
             }
             else
             {
                 //you are an island
-                Console.WriteLine("island");
+                // Console.WriteLine("island");
                 sides += 4;
                 _walkedNodes.Add(nodeInPlot);
                 continue;
@@ -149,8 +149,10 @@ public class Day12 : BaseDay
                 else
                     can we turn right? if yes do so and sides++;
 
-                foreach(subPlot in subPlots)
-                    mainPlotSides += subPlot.sides;
+                This won't work on this example, as we only find the outside perimeter for a sidecount of 4, but the puzzle input wasn't this tricky.
+                AAA
+                ABA
+                AAA
             */
 
             var startingNode = nodeInPlot;
@@ -162,7 +164,7 @@ public class Day12 : BaseDay
                 var advance = currentNode.Edges.Where(edge=>edge.Direction == direction);
                 if(advance.Any())
                 {
-                    Console.WriteLine($"Advance 1 step {direction}");
+                    // Console.WriteLine($"Advance 1 step {direction}");
                     currentNode = advance.First().Node;
                     _walkedNodes.Add(currentNode);
                     var leftDir = direction.TurnLeft();
@@ -171,18 +173,18 @@ public class Day12 : BaseDay
                     {
                         direction = leftDir;
                         sides++;
-                        Console.WriteLine($"Turning left to head {leftDir} and increasing side count to {sides}");
+                        // Console.WriteLine($"Turning left to head {leftDir} and increasing side count to {sides}");
                     }
                 }
                 else
                 {
-                    Console.WriteLine($"Unable to advice 1 step in {direction}, turning right to head {direction.TurnRight()} and increasing side count to {++sides}");
+                    // Console.WriteLine($"Unable to advice 1 step in {direction}, turning right to head {direction.TurnRight()} and increasing side count to {++sides}");
                     direction = direction.TurnRight();
-                    // sides++;
+                    sides++;
                 }
             }
             while (!(currentNode == startingNode && direction == startingDirection));
-            Console.WriteLine("Reached starting point going the same direction");
+            // Console.WriteLine("Reached starting point going the same direction");
         }
 
         return new(nodesInPlot.Count, perimeter, sides);
