5e1370fc442629fa445202137ebd9228ceabe1da
Matt Costuros <matt.costuros@gmail.com>
2024-12-13 10:09:37 -0500

    sad i forgot my linear algebra

--- src/AOC2024/Day13.cs
+++ src/AOC2024/Day13.cs
@@ -1,20 +1,20 @@
 using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 
 public class Day13 : BaseDay
 {
 
-    class MachineDeets
+    class MachineDetail
     {
         public (int x, int y) ButtonA { get; set; }
         public (int x, int y) ButtonB { get; set; }
         public (int x, int y) PrizeLocation { get; set; }
     }
 
-    List<MachineDeets> MachineDetails { get; init; } = [];
+    List<MachineDetail> MachineDetails { get; init; } = [];
     const int ButtonACost = 3;
     const int ButtonBCost = 1;
-    const int MaxPresses = 100;
     public Day13()
     {
         StreamReader sr = new StreamReader(InputFilePath);
@@ -30,7 +30,7 @@ public class Day13 : BaseDay
             MatchCollection buttonBMatches = Regex.Matches(buttonBline, buttonPattern);
             MatchCollection targetMatches = Regex.Matches(targetLine, targetPattern);
 
-            MachineDetails.Add(new MachineDeets
+            MachineDetails.Add(new MachineDetail
             {
                 ButtonA = (int.Parse(buttonAMatches.First().Groups[1].Value), int.Parse(buttonAMatches.First().Groups[2].Value)),
                 ButtonB = (int.Parse(buttonBMatches.First().Groups[1].Value), int.Parse(buttonBMatches.First().Groups[2].Value)),
@@ -47,9 +47,26 @@ public class Day13 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         long output = 0;
-        foreach (var machine in MachineDetails)
+        for (int i = 0; i < MachineDetails.Count; i++)
         {
-            if (CanPlayToWin(machine, out var pressDetails))
+            if (FindWinningCombo(MachineDetails[i], 0, out var pressDetails))
+            {
+                if (pressDetails.ButtonACount <= 100 && pressDetails.ButtonBCount <= 100)
+                {
+                    output += ButtonACost * pressDetails.ButtonACount + ButtonBCost * pressDetails.ButtonBCount;
+                }
+            }
+        }
+
+        return new($"{output}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        long output = 0;
+        for (int i = 0; i < MachineDetails.Count; i++)
+        {
+            if (FindWinningCombo(MachineDetails[i], 10000000000000, out var pressDetails))
             {
                 output += ButtonACost * pressDetails.ButtonACount + ButtonBCost * pressDetails.ButtonBCount;
             }
@@ -58,48 +75,28 @@ public class Day13 : BaseDay
         return new($"{output}");
     }
 
-    private bool CanPlayToWin(MachineDeets machine, out (int ButtonACount, int ButtonBCount) pressDetails)
+    private bool FindWinningCombo(MachineDetail machine, long offset, out (long ButtonACount, long ButtonBCount) pressDetails)
     {
-        var xCanWin = CanPlayToWinButton(machine.PrizeLocation.x, machine.ButtonA.x, machine.ButtonB.x, out var xPressDetails);
-        var yCanWin = CanPlayToWinButton(machine.PrizeLocation.y, machine.ButtonA.y, machine.ButtonB.y, out var yPressDetails);
-        if (xCanWin && yCanWin)
+        var Q = machine.ButtonA.x;
+        var W = machine.ButtonB.x;
+        var E = machine.ButtonA.y;
+        var R = machine.ButtonB.y;
+
+        var b = (E * (machine.PrizeLocation.x + offset) - Q * (machine.PrizeLocation.y + offset)) / ((E * W) - (Q * R));
+        var a1 = (machine.PrizeLocation.x + offset - (b * W)) / Q;
+        var a1d = (machine.PrizeLocation.x + offset - (b * W)) / Convert.ToDouble(Q);
+        var a2 = (machine.PrizeLocation.y + offset - (b * R)) / E;
+
+
+        if (b >= 0 && a1 >= 0 && a1 == a2 && a1 == a1d)
         {
-            var validCombos = xPressDetails.Where(xPress => yPressDetails.Contains(xPress));
-            if (validCombos.Any())
-            {
-                var ordered = validCombos.OrderBy(combo => ButtonACost * combo.ButtonACount + ButtonBCost * combo.ButtonBCount);
-                pressDetails = (ordered.First().ButtonACount, ordered.First().ButtonBCount);
-                return true;
-            }
+            pressDetails = (a1, b);
+            return true;
         }
-        pressDetails = (0, 0);
-        return false;
-    }
-
-    private bool CanPlayToWinButton(int target, int stepA, int stepB, out List<(int ButtonACount, int ButtonBCount)> PressDetails)
-    {
-        PressDetails = [];
-        //Brute. Works for part 1, obviously not for part 2 :)
-        for (int aPress = 1; aPress <= 100; aPress++)
+        else
         {
-            if (aPress * stepA > target) break;
-            for (int bPress = 1; bPress <= 100; bPress++)
-            {
-                var xx = aPress * stepA + bPress * stepB;
-                if (xx > target) break;
-                if (xx == target)
-                {
-                    PressDetails.Add(new(aPress, bPress));
-                }
-            }
+            pressDetails = (0, 0);
+            return false;
         }
-        return PressDetails.Any();
-
     }
-
-    public override ValueTask<string> Solve_2()
-    {
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
-    }
-
 }
