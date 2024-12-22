e46c03b0f3c146755294cfbf62c5873602bd6a1c
Matt Costuros <matt.costuros@gmail.com>
2024-12-21 20:24:40 -0500

    wip

--- src/AOC2024/Day21.cs
+++ src/AOC2024/Day21.cs
@@ -41,10 +41,10 @@ public class Day21 : BaseDay
         bool leftBeforeUp = true;
         bool downBeforeRight = true;
 
-        if(start.row == 4) leftBeforeUp = false;
-        if(start.col==1) downBeforeRight = false;
+        if (start.row == 4 && end.col == 1) leftBeforeUp = false;
+        if (start.col == 1 && end.row == 4) downBeforeRight = false;
 
-        if(leftBeforeUp)
+        if (leftBeforeUp)
         {
             if (colDelta < 0)
                 output.AddRange(Enumerable.Range(1, -colDelta).Select(_ => KeypadCommand.Left).ToList());
@@ -53,14 +53,14 @@ public class Day21 : BaseDay
         if (rowDelta < 0)
             output.AddRange(Enumerable.Range(1, -rowDelta).Select(_ => KeypadCommand.Up).ToList());
 
-        if(!leftBeforeUp)
+        if (!leftBeforeUp)
         {
             if (colDelta < 0)
                 output.AddRange(Enumerable.Range(1, -colDelta).Select(_ => KeypadCommand.Left).ToList());
 
         }
 
-        if(downBeforeRight)
+        if (downBeforeRight)
         {
             if (rowDelta > 0)
                 output.AddRange(Enumerable.Range(1, rowDelta).Select(_ => KeypadCommand.Down).ToList());
@@ -70,34 +70,11 @@ public class Day21 : BaseDay
             output.AddRange(Enumerable.Range(1, colDelta).Select(_ => KeypadCommand.Right).ToList());
 
 
-        if(!downBeforeRight)
+        if (!downBeforeRight)
         {
             if (rowDelta > 0)
                 output.AddRange(Enumerable.Range(1, rowDelta).Select(_ => KeypadCommand.Down).ToList());
         }
-
-        // bool colFirst = start.row != 4;
-
-        // if (colFirst)
-        // {
-        //     if (colDelta > 0)
-        //         output.AddRange(Enumerable.Range(1, colDelta).Select(_ => KeypadCommand.Right).ToList());
-        //     if (colDelta < 0)
-        //         output.AddRange(Enumerable.Range(1, -colDelta).Select(_ => KeypadCommand.Left).ToList());
-        // }
-
-        // if (rowDelta > 0)
-        //     output.AddRange(Enumerable.Range(1, rowDelta).Select(_ => KeypadCommand.Down).ToList());
-        // if (rowDelta < 0)
-        //     output.AddRange(Enumerable.Range(1, -rowDelta).Select(_ => KeypadCommand.Up).ToList());
-
-        // if (!colFirst)
-        // {
-        //     if (colDelta > 0)
-        //         output.AddRange(Enumerable.Range(1, colDelta).Select(_ => KeypadCommand.Right).ToList());
-        //     if (colDelta < 0)
-        //         output.AddRange(Enumerable.Range(1, -colDelta).Select(_ => KeypadCommand.Left).ToList());
-        // }
 
         return output;
     }
@@ -124,13 +101,9 @@ public class Day21 : BaseDay
         (int row, int col) end = FindRobotKeypadLocation(c2);
         var rowDelta = end.row - start.row;
         var colDelta = end.col - start.col;
-        
-    
+
+
         // Left before Up, Down before Right, unless the gap is in the way
-        bool leftBeforeUp = true;
-        bool rightBeforeDown = true;
-        
-        
         bool leftArrowEnd = end.col == 1;
         bool leftArrowStart = start.col == 1;
 
@@ -139,25 +112,13 @@ public class Day21 : BaseDay
             if (rowDelta > 0)
                 output.AddRange(Enumerable.Range(1, rowDelta).Select(_ => KeypadCommand.Down).ToList());
 
-            // if (rowDelta < 0)
-            //     output.AddRange(Enumerable.Range(1, -rowDelta).Select(_ => KeypadCommand.Up).ToList());
-
-            // if (colDelta > 0)
-            //     output.AddRange(Enumerable.Range(1, colDelta).Select(_ => KeypadCommand.Right).ToList());
-
             if (colDelta < 0)
                 output.AddRange(Enumerable.Range(1, -colDelta).Select(_ => KeypadCommand.Left).ToList());
         }
-        else if(leftArrowStart)
+        else if (leftArrowStart)
         {
             if (colDelta > 0)
                 output.AddRange(Enumerable.Range(1, colDelta).Select(_ => KeypadCommand.Right).ToList());
-
-            // if (colDelta < 0)
-            //     output.AddRange(Enumerable.Range(1, -colDelta).Select(_ => KeypadCommand.Left).ToList());
-
-            // if (rowDelta > 0)
-            //     output.AddRange(Enumerable.Range(1, rowDelta).Select(_ => KeypadCommand.Down).ToList());
 
             if (rowDelta < 0)
                 output.AddRange(Enumerable.Range(1, -rowDelta).Select(_ => KeypadCommand.Up).ToList());
@@ -173,7 +134,6 @@ public class Day21 : BaseDay
                 output.AddRange(Enumerable.Range(1, rowDelta).Select(_ => KeypadCommand.Down).ToList());
             if (colDelta > 0)
                 output.AddRange(Enumerable.Range(1, colDelta).Select(_ => KeypadCommand.Right).ToList());
-
         }
 
         return output;
@@ -220,8 +180,6 @@ public class Day21 : BaseDay
         long output = 0;
         checked
         {
-
-
             foreach (var line in _lines)
             {
                 var doorRobotCommands = GenerateDoorRobotCommands(line);
@@ -234,14 +192,14 @@ public class Day21 : BaseDay
                 for (int i = 0; i < 2; i++)
                 {
                     Commands = GenerateKeypadRobotCommands(Commands);
-                    Console.WriteLine($"Code {line} Keypad {i + 1} Commands");
-                    Console.WriteLine($"{string.Join(string.Empty, Commands.Select(cmd => cmd.ToPrintString()))}");
+                    // Console.WriteLine($"Code {line} Keypad {i + 1} Commands");
+                    // Console.WriteLine($"{string.Join(string.Empty, Commands.Select(cmd => cmd.ToPrintString()))}");
                     ValidateKeypadRobotCommands(Commands);
                 }
                 var code = int.Parse(line.Replace("A", string.Empty).Trim());
                 long complexity = Commands.Count * code;
                 Console.WriteLine($"Input {line} Code {code} Command Count {Commands.Count} Complexity {complexity}");
-                Console.WriteLine($"{line}: {string.Join(string.Empty, Commands.Select(c => c.ToPrintString()))}");
+                // Console.WriteLine($"{line}: {string.Join(string.Empty, Commands.Select(c => c.ToPrintString()))}");
                 output += complexity;
 
             }
@@ -312,7 +270,97 @@ public class Day21 : BaseDay
 
     public override ValueTask<string> Solve_2()
     {
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        long output = 0;
+        Dictionary<(KeypadCommand start, KeypadCommand end), List<KeypadCommand>> Expansion = GenerateExpansion();
+        //needs to be Start,end, int
+        Dictionary<(KeypadCommand start, KeypadCommand end), int> CommandCount = new();
+
+        checked
+        {
+            foreach (var line in _lines)
+            {
+                var doorRobotCommands = GenerateDoorRobotCommands(line);
+                Console.WriteLine($"Code {line} Door Robot Commands");
+                Console.WriteLine($"{string.Join(string.Empty, doorRobotCommands.Select(cmd => cmd.ToPrintString()))}");
+
+                var firstGenCommands = GenerateKeypadRobotCommands(doorRobotCommands);
+                for (int i = 0; i < firstGenCommands.Count - 1; i++)
+                {
+                    var key = (firstGenCommands[i], firstGenCommands[i+1]);
+                    if(!CommandCount.ContainsKey(key))
+                    {
+                        CommandCount[key]=1;
+                    }
+                    else
+                    {
+                        CommandCount[(firstGenCommands[i], firstGenCommands[i + 1])]++;
+                    }
+                }
+
+                for (int i = 0; i < 25; i++)
+                {
+                    var currentCounts = CommandCount.ToArray();
+                    CommandCount.Clear();
+                    foreach (var ckvp in currentCounts)
+                    {
+                        var expandedCmd = Expansion[ckvp.Key];
+                        for (int x = 0; x < expandedCmd.Count - 1; x++)
+                        {
+                            var cmd = (expandedCmd[x], expandedCmd[x+1]);
+                            if(!CommandCount.ContainsKey(cmd))
+                            {
+                                CommandCount[cmd] = ckvp.Value;
+                            }
+                            else
+                            {
+                                CommandCount[cmd] += ckvp.Value;
+                            }
+                        }
+                    }
+                }
+                 
+                var totalCmdCount = CommandCount.Select(kvp => kvp.Value * Expansion[kvp.Key].Count).Sum();
+                var code = int.Parse(line.Replace("A", string.Empty).Trim());
+                long complexity = totalCmdCount * code;
+                Console.WriteLine($"Input {line} Code {code} Command Count {totalCmdCount} Complexity {complexity}");
+                // Console.WriteLine($"{line}: {string.Join(string.Empty, Commands.Select(c => c.ToPrintString()))}");
+                output += complexity;
+
+            }
+        }
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2 {output}");
+
     }
 
+    private Dictionary<(KeypadCommand start, KeypadCommand end), List<KeypadCommand>> GenerateExpansion()
+    {
+        return new Dictionary<(KeypadCommand start, KeypadCommand end), List<KeypadCommand>>
+        {
+            [(KeypadCommand.Activate, KeypadCommand.Left)] = GenerateKeypadRobotCommand(KeypadCommand.Activate, KeypadCommand.Left),
+            [(KeypadCommand.Activate, KeypadCommand.Right)] = GenerateKeypadRobotCommand(KeypadCommand.Activate, KeypadCommand.Right),
+            [(KeypadCommand.Activate, KeypadCommand.Up)] = GenerateKeypadRobotCommand(KeypadCommand.Activate, KeypadCommand.Up),
+            [(KeypadCommand.Activate, KeypadCommand.Down)] = GenerateKeypadRobotCommand(KeypadCommand.Activate, KeypadCommand.Down),
+
+            [(KeypadCommand.Up, KeypadCommand.Left)] = GenerateKeypadRobotCommand(KeypadCommand.Up, KeypadCommand.Left),
+            [(KeypadCommand.Up, KeypadCommand.Right)] = GenerateKeypadRobotCommand(KeypadCommand.Up, KeypadCommand.Right),
+            [(KeypadCommand.Up, KeypadCommand.Activate)] = GenerateKeypadRobotCommand(KeypadCommand.Up, KeypadCommand.Activate),
+            [(KeypadCommand.Up, KeypadCommand.Down)] = GenerateKeypadRobotCommand(KeypadCommand.Up, KeypadCommand.Down),
+
+            [(KeypadCommand.Down, KeypadCommand.Left)] = GenerateKeypadRobotCommand(KeypadCommand.Down, KeypadCommand.Left),
+            [(KeypadCommand.Down, KeypadCommand.Right)] = GenerateKeypadRobotCommand(KeypadCommand.Down, KeypadCommand.Right),
+            [(KeypadCommand.Down, KeypadCommand.Up)] = GenerateKeypadRobotCommand(KeypadCommand.Down, KeypadCommand.Up),
+            [(KeypadCommand.Down, KeypadCommand.Activate)] = GenerateKeypadRobotCommand(KeypadCommand.Down, KeypadCommand.Activate),
+
+            [(KeypadCommand.Left, KeypadCommand.Activate)] = GenerateKeypadRobotCommand(KeypadCommand.Left, KeypadCommand.Activate),
+            [(KeypadCommand.Left, KeypadCommand.Right)] = GenerateKeypadRobotCommand(KeypadCommand.Left, KeypadCommand.Right),
+            [(KeypadCommand.Left, KeypadCommand.Up)] = GenerateKeypadRobotCommand(KeypadCommand.Left, KeypadCommand.Up),
+            [(KeypadCommand.Left, KeypadCommand.Down)] = GenerateKeypadRobotCommand(KeypadCommand.Left, KeypadCommand.Down),
+
+            [(KeypadCommand.Right, KeypadCommand.Left)] = GenerateKeypadRobotCommand(KeypadCommand.Right, KeypadCommand.Left),
+            [(KeypadCommand.Right, KeypadCommand.Activate)] = GenerateKeypadRobotCommand(KeypadCommand.Right, KeypadCommand.Activate),
+            [(KeypadCommand.Right, KeypadCommand.Up)] = GenerateKeypadRobotCommand(KeypadCommand.Right, KeypadCommand.Up),
+            [(KeypadCommand.Right, KeypadCommand.Down)] = GenerateKeypadRobotCommand(KeypadCommand.Right, KeypadCommand.Down),
+        };
+
+    }
 }
