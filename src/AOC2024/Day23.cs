68e2d752714a3e51fd07e079ab51f815ef9150df
Matt Costuros <matt.costuros@gmail.com>
2024-12-23 09:37:27 -0500

    part2

--- src/AOC2024/Day23.cs
+++ src/AOC2024/Day23.cs
@@ -54,10 +54,10 @@ public class Day23 : BaseDay
         var triForce = Find3Ways();
         Console.WriteLine($"There are {triForce.Count} 3 ways to search");
         int output = 0;
-        foreach(var triplet in triForce)
+        foreach (var triplet in triForce)
         {
-            if(triplet.Item1.StartsWith("t") || triplet.Item2.StartsWith("t") || triplet.Item3.StartsWith("t") )
-                output ++;
+            if (triplet.Item1.StartsWith("t") || triplet.Item2.StartsWith("t") || triplet.Item3.StartsWith("t"))
+                output++;
         }
         return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 {output}");
     }
@@ -67,17 +67,17 @@ public class Day23 : BaseDay
         var output = new List<(string, string, string)>();
         foreach (var nerd in nerds)
         {
-            for (int i = 0; i < nerd.Edges.Count -1 ; i++)
+            for (int i = 0; i < nerd.Edges.Count - 1; i++)
             {
-                for (int j = 1; j < nerd.Edges.Count ; j++)
+                for (int j = 1; j < nerd.Edges.Count; j++)
                 {
                     var linked1 = nerd.Edges[i];
                     var linked2 = nerd.Edges[j];
-                    if(linked1.Edges.Contains(linked2))
+                    if (linked1.Edges.Contains(linked2))
                     {
-                        var tri = new List<string> {nerd.Value, linked1.Value, linked2.Value}.Order().ToList();
+                        var tri = new List<string> { nerd.Value, linked1.Value, linked2.Value }.Order().ToList();
                         var x = (tri[0], tri[1], tri[2]);
-                        if(!output.Contains(x))
+                        if (!output.Contains(x))
                         {
                             output.Add(x);
                         }
@@ -89,7 +89,70 @@ public class Day23 : BaseDay
     }
     public override ValueTask<string> Solve_2()
     {
-        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        Console.WriteLine($"Still have {nerds.Count} computers to search");
+        var output = FindLanParty();
+        return new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1 `{output}`");
+    }
+
+    private bool MutuallyLinked(IEnumerable<LinkedNode<string>> nodes)
+    {
+        foreach (var node in nodes)
+        {
+            var targetLinkValues = nodes.Where(n => n.Value != node.Value).Select(n => n.Value);
+            var matches = node.Edges.Count(e => targetLinkValues.Contains(e.Value));
+            if (matches != targetLinkValues.Count()) return false;
+        }
+        return true;
+
+    }
+    private string FindLanParty()
+    {
+        var output = string.Empty;
+        int largestGroup = 0;
+        foreach (var nerd in nerds)
+        {
+            var possibleLans = GenerateCombos(nerd, largestGroup + 1).ToList();
+            foreach (var possibleLan in possibleLans)
+            {
+                if (MutuallyLinked(possibleLan))
+                {
+                    if (possibleLan.Count() > largestGroup)
+                    {
+                        largestGroup = possibleLan.Count();
+                        //update output
+                        output = string.Join(",", possibleLan.Select( x=>x.Value).Order());
+                    }
+                }
+            }
+        }
+        return output;
+    }
+
+
+    IEnumerable<IEnumerable<LinkedNode<string>>> GenerateCombos(LinkedNode<string> node, int minSize)
+    {
+        var output = new List<IEnumerable<LinkedNode<string>>>();
+        var largestPossible = node.Edges.Count + 1;
+        var options = node.Edges.ToList();
+        options.Add(node);
+        for (int i = minSize; i <= largestPossible; i++)
+        {
+            var newCombos = options.Combinations(i).ToList();
+            foreach(var combo in newCombos)
+                output.Add(combo);
+        }
+        return output;
+    }
+
+}
+
+public static class ComboExtensions
+{
+    public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
+    {
+        return k == 0 ? new[] { new T[0] } :
+        elements.SelectMany((e, i) =>
+        elements.Skip(i + 1).Combinations(k - 1).Select(c => (new[] { e }).Concat(c)));
     }
 
 }
