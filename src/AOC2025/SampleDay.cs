c83b10aeaeb6d1715a2be53867c6a0f17905f4db
mattcosturos <mcosturos@moog.com>
2025-12-05 15:16:28 -0500

    Add sample day

---
+++ src/AOC2025/SampleDay.cs
@@ -0,0 +1,31 @@
+// public class DayXX : BaseDay
+// {
+//     List<string> _lines = new();
+
+//     public DayXX()
+//     {
+//         StreamReader sr = new StreamReader(InputFilePath);
+
+//         string? line;
+//         while ((line = sr.ReadLine()) != null)
+//         {
+//             _lines.Add(line);
+//         }
+
+//     }
+
+//     public override ValueTask<string> Solve_1()
+//     {
+//         long output = 0;
+
+//         return new($"{output}");
+//     }
+
+//     public override ValueTask<string> Solve_2()
+//     {
+//         long output = 0;
+
+//         return new($"{output}");
+//     }
+
+// }
