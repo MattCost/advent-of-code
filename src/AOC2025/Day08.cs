720423570527c70bdcd2e6e41a5d83c9bf0f4d44
mattcosturos <mcosturos@moog.com>
2025-12-08 10:23:58 -0500

    Day 8 - part 1 complete

--- src/AOC2025/Day08.cs
+++ src/AOC2025/Day08.cs
@@ -27,6 +27,22 @@ public class Day08 : BaseDay
         }
     }
 
+    private class JunctionBoxNetwork
+    {
+        public List<JunctionBox> Nodes { get; set; } = new();
+        public void AddJunctionBox(JunctionBox node)
+        {
+            Nodes.Add(node);
+        }
+        public bool ContainsJunctionBox(JunctionBox node)
+        {
+            return Nodes.Contains(node);
+        }
+        public void AddNetwork(JunctionBoxNetwork network)
+        {
+            Nodes.AddRange(network.Nodes);
+        }
+    }
     List<JunctionBox> junctionBoxes = new();
     public Day08()
     {
@@ -44,25 +60,72 @@ public class Day08 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         long output = 0;
-        /*
-            while link counter < 10
-                get closest 2 boxes
-                if not in a network, link them, linkCounter++
 
-            this means I need a distance Map, and a network tracker
-        */
-
-        List< (double Distance, JunctionBox Node1, JunctionBox Node2)> distanceTracker = new();
-        for(int i = 0 ; i< junctionBoxes.Count-1 ; i++)
+        List<(double Distance, JunctionBox Node1, JunctionBox Node2)> distanceTracker = new();
+        for (int i = 0; i < junctionBoxes.Count - 1; i++)
         {
-            for(int j=i+1 ; j<junctionBoxes.Count; j++)
+            for (int j = i + 1; j < junctionBoxes.Count; j++)
             {
-                distanceTracker.Add( new(junctionBoxes[i].DistanceTo(junctionBoxes[j]), junctionBoxes[i], junctionBoxes[j]));
+                distanceTracker.Add(new(junctionBoxes[i].DistanceTo(junctionBoxes[j]), junctionBoxes[i], junctionBoxes[j]));
             }
         }
         Console.WriteLine("Distance Map Calculated");
-        distanceTracker = distanceTracker.OrderBy(x=>x.Distance).ToList();
+        distanceTracker = distanceTracker.OrderBy(x => x.Distance).ToList();
 
+        List<JunctionBoxNetwork> networkTracker = new();
+
+        int linkCounter = 0;
+        int distanceIndex = 0;
+        while (linkCounter < 1000)
+        {
+            // Get closest 2 boxes;
+            var node1 = distanceTracker[distanceIndex].Node1;
+            var node2 = distanceTracker[distanceIndex].Node2;
+            distanceIndex++;
+
+            // Search for networks
+            var node1Network = networkTracker.Where(network => network.ContainsJunctionBox(node1));
+            var node2Network = networkTracker.Where(network => network.ContainsJunctionBox(node2));
+
+            // The puzzle description is confusing "nothing happens" implies you don't need to make a link, but it seems to mean nothing happens to the circuit, as it was already complete
+            // which means this could have been a for loop
+            linkCounter++;
+            
+            // Nodes already in same network
+            if (node1Network.Any() && node2Network.Any() && node1Network.Single() == node2Network.Single())
+            {
+                continue;
+            }
+
+            //Node 1 in network, node 2 in network -> add node 2's network to node 1's network, and delete node 2's network
+            if(node1Network.Any() && node2Network.Any())
+            {
+                var node1N = node1Network.Single();
+                var node2N = node2Network.Single();
+                node1N.AddNetwork(node2N);
+                networkTracker.Remove(node2N);
+            } 
+            //Node 1 IN network, node 2 not in network  -> add node 2 to node 1's network
+            else if(node1Network.Any() && !node2Network.Any())
+            {
+                node1Network.Single().AddJunctionBox(node2);
+            }
+            else if(node2Network.Any() && !node1Network.Any())
+            {
+                node2Network.Single().AddJunctionBox(node1);
+            }
+            //Node 1 NOT in network, node 2 NOT in netwrk -> create new network
+            else
+            {
+                var network = new JunctionBoxNetwork();
+                network.AddJunctionBox(node1);
+                network.AddJunctionBox(node2);
+                networkTracker.Add(network);
+            }
+        }
+
+        var x = networkTracker.OrderByDescending(network => network.Nodes.Count).Take(3).Select(network => network.Nodes.Count).ToList();
+        output = x[0] * x[1] * x[2];
 
         return new($"{output}");
     }
